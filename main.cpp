cc0102eff672d3e739369f8346d20de4fc0c765f
salmanakmal1609 <salmanakmal1609@gmail.com>
2026-01-02 23:02:41 +0500

    Made console menu and done variable declaration.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,74 @@
 using namespace std;
 int main()
 {
-  
-  return 0;
+  //Salman's Part.
+  //Variables for Data Entry
+  const int maxstudents=30;
+  char student_name[maxstudents];
+  char Section[maxstudents];
+  char Department[10];
+  int Semester[10];
+  //____________________________________________
+  //Variable for choice and current count of students
+  int choice=-1;
+  //Infinite Loop for Menu
+  bool infinite_loop=true;
+  do{
+  //Menu Display
+    cout << "============================================================\n";
+    cout << " Quiz Marks & Result Summary System\n";
+    cout << "============================================================\n";
+    cout << "1) Enter / Replace Student Records\n";
+    cout << "2) Enter / Replace Quiz Setup (Total Marks + Date)\n";
+    cout << "3) Enter Quiz Marks (Student-wise or Quiz-wise)\n";
+    cout << "4) Update Marks (Student ID + Quiz)\n";
+    cout << "5) Display Complete Marks Sheet\n";
+    cout << "6) Display Individual Student Result Card\n";
+    cout << "7) Quiz-wise Statistics\n";
+    cout << "8) Generate Ranking / Top Performers\n";
+    cout << "9) Search / Listings\n";
+    cout << "10) Validate Data & Detect Issues\n";
+    cout << "11) Generate Reports / Summaries\n";
+    cout << "0) Exit\n";
+    cout << "--------------------------------------------\n";
+    cout << "Enter your choice: ";
+    cin>>choice;
+    cout << "--------------------------------------------\n";
+  if(choice==1){
+    int current_studentcount=0;
+    cout<<"Enter Number of Students: ";
+    cin>>current_studentcount;
+    if (current_studentcount<10||current_studentcount>maxstudents)
+    {
+      cout<<"Invalid Number of Students. Please try again."<<endl;
+      continue;
+    }
+    else{
+    for(int i=0;i<current_studentcount;i++)
+    {
+      cout<<"Enter Student Name: ";
+      cin>>student_name[i];
+      cout<<"Enter Section: ";
+      cin>>Section[i];
+      cout<<"Enter Department: ";
+      cin>>Department[i];
+      cout<<"Enter Semester: ";
+      cin>>Semester[i];
+    }}
+  }
+  else if (choice==2){}
+  else if (choice==3){}
+  else if (choice==4){}
+  else if (choice==5){}
+  else if (choice==6){}
+  else if (choice==7){}
+  else if (choice==8){}
+  else if (choice==9){}
+  else if (choice==0)break;
+  else
+  {
+    cout<<"Invalid Choice. Please try again."<<endl;
+  }
+  }while(infinite_loop);
+  return 0; 
 }
